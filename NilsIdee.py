07f0a06fce27a53e0bc6aba37572240320d137c6
nils <nils.schiele@uni-konstanz.de>
2024-12-02 15:11:41 +0100

    Earthworm love is cudly ^^

--- NilsIdee.py
+++ NilsIdee.py
@@ -1,10 +1,13 @@
 # Assignment 5: Battle of the 5 "or less" armies
 
+#TODO Kommentare übersetzen...DAS IST ALLES NOCH TEMPORÄR ALSO NICHT RUMMAULEN DANIEL ^^
+
 import random
 
 #---------------------------------------------
-# assets
+# "Game assets" -> Heroes, Weapons, Monsters
 
+#TODO Beschreibungen und Werte anpassen
 # Helden
 # health, strength, luck durch Werte austauschen
 Aragorn = ("Aragorn", "Beschreibung", "health", "strength", 3) 
@@ -49,44 +52,80 @@ while monsterCount > 0:
     monsterCount -= 1
 
 #---------------------------------------------
+# Game 
+# Explanation of the game:
+# TODO: Erklärung des Spiels einfügen
 
-# Explanation text
+#---------------------------------------------
+# Lore/Story
+# TODO: Lore/Story einfügen
 
-# Schleife zur Heldenwahl -> User wird liste mit helden angezeigt mit entsprechenden Buchstaben kann er sich mehr Informationen anzeigen lassen -> In Informationen Screen kann er zurück zu auswahl oder game starten
-x = "werDasLiestIstDoof"
-while x != "x":
-    
-    print("Welcome to the Battle of the 5 (or less) Armies!")
-    print("You can choose between the following heroes:")
-    
+print("Welcome to the Battle of the 5 (or less) Armies!")
+print("You can choose between the following heroes:")
+
+
+#---------------------------------------------
+# Loop to choose a hero
+# Choosable heroes are "Aragorn", "Gimli", "Legolas", "Frodo" and "Boromir"
+# If the user types the name of the hero, the hero is chosen
+# If the user types the number of the hero, the user gets more information about the hero
+# If the user types "x" the game is exited
+
+chosenHero = None
+while chosenHero == None:
+        
     i=0
     for hero in heroes:
         i+=1
         print(hero[0]+ " (" + str(i) + ")")
-    x = input("Choose your hero with (1-5) or exit(x): ")
 
+    print("To choose a hero type the name of the hero")
+    x = input("See more information of a hero type the corresponding number (1-5) or exit with (x): ")
 
     match x:
-    case "1":
-        print("Text Aragorn")
-    case "2":
-        print("Text Gimli")
-    case "3":
-        print("Text Legolas")
-    case "4":
-        print("Text Frodo")
-    case "5":
-        print("Text Boromir")
-    case _:
-        print("wrong input")
+        case "1":
+            print("\nText Aragorn\n")
+        case "2":
+            print("\nText Gimli\n")
+        case "3":
+            print("\nText Legolas\n")
+        case "4":
+            print("\nText Frodo\n")
+        case "5":
+            print("\nText Boromir\n")
+        case "x":
+            print("See you soon ^^ ")
+            break
+
+        case "Aragorn":
+            chosenHero = Aragorn
+            break
+        case "Gimli":
+            chosenHero = Gimli
+            break
+        case "Legolas":
+            chosenHero = Legolas
+            break
+        case "Frodo":
+            chosenHero = Frodo
+            break
+        case "Boromir":
+            chosenHero = Boromir
+            break  
+
+        case _:
+            print("wrong input")
+
+#---------------------------------------------    
 
               
 
-# Text -> Du begegnest x Monstern 
-print("Du begegnest", len(monsterList), "Monstern") #anpassen
 
 
-# Battle Schleife solange noch Monster in der Liste wähle erstes und simuliere Kampf -> wenn Monster tot entferne es aus Liste, sonst losingscreen
+#---------------------------------------------
+# Battle Loop
+
+# TODO Battle Schleife solange noch Monster in der Liste wähle erstes und simuliere Kampf -> wenn Monster tot entferne es aus Liste, sonst losingscreen
 while len(monsterList) > 0: 
     monster = monsterList[0]
     # Kampf simulieren
