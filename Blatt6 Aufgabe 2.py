cc36d8163ea30d336d664dc244c810d3de068480
nils <nils.schiele@uni-konstanz.de>
2024-12-16 14:24:05 +0100

    Komentare Aufgabe 2

--- Blatt6 Aufgabe 2.py
+++ Blatt6 Aufgabe 2.py
@@ -1,8 +1,14 @@
+#---------------------------------------------------------
+# Exercise 2
+
+# add the given dictionaries:
 dict1 = {"Harry": 3, "the": 8, "laugh": 3}
 dict2 = {"Harry": 4, "the": 6, "laugh": 1}
 
+# check the keys of the dictionaries and add the values of the same keys and print the result to the console
 for key in dict1:
     for key2 in dict2:
         if key == key2:
             x = dict1[key] + dict2[key2]
             print(x)
+#---------------------------------------------------------
