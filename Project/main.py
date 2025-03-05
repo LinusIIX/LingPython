b6a583a790f12bbee70d3a9411e07f518f9c4212
Luca Pomm <luca.pomm@uni-konstanz.de>
2025-03-05 15:51:15 +0100

    relativ pos player move

--- Project/main.py
+++ Project/main.py
@@ -18,21 +18,25 @@ class EventStone(Node):
         self.displayName = self.font.render(self.moduleName, True, (0, 255, 255), (0, 0, 255))
         self.nameRect = self.displayName.get_rect()
         self.rect_size = (self.nameRect[2], self.nameRect[3])
-        self.nameRect.center = (self.position[0], self.position[1])
-        self.position = (self.position[0] - self.rect_size[0] * 0.5, self.position[1] - self.rect_size[1] * 0.5)
 
     def process(self, dp):
-        pygame.draw.rect(dp, (50, 50, 50), (self.position[0], self.position[1], self.rect_size[0], self.rect_size[1]))
+        pygame.draw.rect(dp, (50, 50, 50), (self.getX(), self.getY(), self.rect_size[0], self.rect_size[1]))
+        self.nameRect.center = (self.getX() + self.rect_size[0] * 0.5, self.getY() + self.rect_size[1] * 0.5)
         dp.blit(self.displayName, self.nameRect)
 
 engine = Engine()
-player = Player(handlesEvents=True)
+root = Node(handlesEvents=False, nodeRefs={}, callProcess=False)
+player = Player(handlesEvents=True, nodeRefs={
+    "root" : root
+})
 player.rect_size = (globals.game_size*player.sprite_rect.width,globals.game_size*player.sprite_rect.height)
 print(player.rect_size)
 i = 100
 for gameEntry in Engine.get_main_files("games"):
-    print(gameEntry)
-    engine.add_node(EventStone(gameEntry, (300, i)))
+    if globals.debug:
+        print(gameEntry)
+    root.add_node(EventStone(gameEntry, (300, i)))
     i += 100
+engine.add_node(root)
 engine.add_node(player)
 engine.run()
