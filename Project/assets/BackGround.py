e1286dcef576412950e0c60727bd6bb3a912df8c
LinusIIX <linus.prange@gmail.com>
2025-03-09 18:58:37 +0100

    (+10,+3)

--- Project/assets/BackGround.py
+++ Project/assets/BackGround.py
@@ -17,21 +17,30 @@ class BackGround(Node):
         self.sprite = pygame.transform.scale(self.sprite, (globals.game_size * self.sprite_size.width, globals.game_size * self.sprite_size.height))
         self.rect = self.sprite.get_rect()
         self.rect = self.rect.move((self.rect.height)/2,(self.rect.width)/2) #Nicht anfassen, funktioniert ((dimension of image)/2)
-        self.tileWidth = (self.rect.width/17)
+        self.tileWidth = (self.rect.width/30)
 
         self.obstacle_map = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
-            [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-            [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             ]
         for i in range(len(self.obstacle_map)):  # Loop through rows
             for j in range(len(self.obstacle_map[i])):  # Loop through columns
@@ -51,37 +60,37 @@ class BackGround(Node):
         self.add_node(interactable_container)
 
         altar_1 = Interactable(description="altar_1", nodeRefs=self.nodeRefs, pickup=False)
-        altar_1.position = ( -self.rect.top + self.tileWidth * 3.3,-self.rect.left + self.tileWidth * 2.2)
+        altar_1.position = ( -self.rect.top + self.tileWidth * 13.3,-self.rect.left + self.tileWidth * 5.2)
         altar_1.rect_size = (self.tileWidth * 1.5,self.tileWidth * 1.5)
         self.add_node(altar_1)
         print(altar_1.position)
 
         altar_2 = Interactable(description = "altar_2", nodeRefs=self.nodeRefs, pickup=False)
-        altar_2.position = ( -self.rect.top + self.tileWidth * 6.9,-self.rect.left + self.tileWidth * 2.2)
+        altar_2.position = ( -self.rect.top + self.tileWidth * 16.9,-self.rect.left + self.tileWidth * 5.2)
         altar_2.rect_size = (self.tileWidth * 1.5,self.tileWidth * 1.5)
         self.add_node(altar_2)
 
         frog = Interactable(description = "frog", nodeRefs=self.nodeRefs, pickup=True,requires="net")
         frogES = EventStone.EventStone({"moduleFolderName" : "froggame", "consoleRun" : False, "modulePath" : "games/froggame/main.py"}, description="get a net to catch the frog")
         self.nodeRefs["eventStoneContainer"].add_node(frogES)
-        frog.position = ( -self.rect.top + self.tileWidth * 8,-self.rect.left + self.tileWidth * 8)
-        frogES.position = ( -self.rect.top + self.tileWidth * 8,-self.rect.left + self.tileWidth * 8)
+        frog.position = ( -self.rect.top + self.tileWidth * 18,-self.rect.left + self.tileWidth * 11)
+        frogES.position = ( -self.rect.top + self.tileWidth * 18,-self.rect.left + self.tileWidth * 11)
         frog.rect_size = (self.tileWidth * 1.5,self.tileWidth * 2.5)
         frogES.rect_size = (self.tileWidth * 1.5,self.tileWidth * 2.5)
         self.add_node(frog)
 
         fish = Interactable(description = "fish", nodeRefs=self.nodeRefs, pickup=True)
-        fish.position = ( -self.rect.top + self.tileWidth * 12,-self.rect.left + self.tileWidth * 4.2)
+        fish.position = ( -self.rect.top + self.tileWidth * 22,-self.rect.left + self.tileWidth * 7.2)
         fish.rect_size = (self.tileWidth * 3,self.tileWidth * 1.5)
         self.add_node(fish)
 
         net = Interactable(description = "net", nodeRefs=self.nodeRefs, pickup=True)
-        net.position = ( -self.rect.top + self.tileWidth * 2,-self.rect.left + self.tileWidth * 9.5)
+        net.position = ( -self.rect.top + self.tileWidth * 12,-self.rect.left + self.tileWidth * 12.5)
         net.rect_size = (self.tileWidth * 1.5,self.tileWidth * 1.5)
         self.add_node(net)
 
         fireplace = Interactable(description = "fireplace", nodeRefs=self.nodeRefs, pickup=False)
-        fireplace.position = ( -self.rect.top + self.tileWidth * 3.3,-self.rect.left + self.tileWidth * 5.7)
+        fireplace.position = ( -self.rect.top + self.tileWidth * 13.3,-self.rect.left + self.tileWidth * 8.7)
         fireplace.rect_size = (self.tileWidth * 1.5,self.tileWidth * 1.5)
         self.add_node(fireplace)
         #interactable_container.add_node(altar_1)
